196e245fe64ff1149654cf27a2b96926594c48d7
Jeon <aaronswagd@gmail.com>
2026-04-21 15:51:13 -0600

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -33,3 +33,22 @@ elements.forEach(el => {
   el.style.transform = "translateY(40px)";
   observer.observe(el);
 });
+
+// El botón detecte el dispositivo y abra lo mejor automáticamente en correo electrónico
+function openEmail(e){
+  e.preventDefault();
+
+  const email = "makne.info@gmail.com";
+  const subject = "Quiero cotizar";
+  const body = "Hola, me interesa tu trabajo";
+
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if(isMobile){
+    // 📱 Móvil → app de correo
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  } else {
+    // 💻 Desktop → Gmail web
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`, "_blank");
+  }
+}
